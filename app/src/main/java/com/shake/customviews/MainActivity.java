5ed13387f3a3dcced2d9e02ac67dde1b9025a3b4
mac <350258655@qq.com>
2017-06-01 10:02:49 +0800

    自定义加减View

---
+++ app/src/main/java/com/shake/customviews/MainActivity.java
@@ -0,0 +1,29 @@
+package com.shake.customviews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnaddsub = (Button) findViewById(R.id.btn_add_sub);
+        btnaddsub.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.btn_add_sub:
+                startActivity(new Intent(MainActivity.this,NumberAddSubActivity.class));
+                break;
+        }
+    }
+}
