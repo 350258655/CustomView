0be20250faed1c9ef68e713db9ba686287acbdc4
mac <350258655@qq.com>
2017-06-09 21:27:12 +0800

    自定义开关

---
+++ app/src/main/java/com/shake/customviews/toggle/ToggleButtonActivity.java
@@ -0,0 +1,26 @@
+package com.shake.customviews.toggle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CompoundButton;
+
+import com.shake.customviews.R;
+
+public class ToggleButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_switch);
+        ToggleButtonView toggle = (ToggleButtonView) findViewById(R.id.toggle);
+        toggle.setChecked(true);
+
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.i("TAG", "当前的状态是："+isChecked);
+            }
+        });
+    }
+}
